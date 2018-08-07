d8509b164ed5940fae8702a304dc1873674ca2aa
vejitta <vejitta123@gmail.com>
2018-08-07 21:03:10 +0300

    Exercise 2.27 Checkerboard Pattern of Asterisks - added a for loop

--- Exercise 2 - 27/Checkerboard.java
+++ Exercise 2 - 27/Checkerboard.java
@@ -9,6 +9,14 @@ public class Checkerboard {
 		System.out.println(" * * * * * * * *");
 		System.out.println("* * * * * * * *");
 		System.out.println(" * * * * * * * *");
+		System.out.println("\n");
+		int i=0;
+		for(i=0; i<8; i++){
+			if (i%2==0)
+				System.out.println("* * * * * * * *"); else
+				System.out.println(" * * * * * * * *");
+		
+		}
 		
 	}
 }
