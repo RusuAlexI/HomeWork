51c77608467ffe7d473ba283a0e40a901f024edf
vejitta <vejitta123@gmail.com>
2018-08-07 20:33:20 +0300

    Exercise 2.27 Checkerboard Pattern of Asterisks

---
+++ Exercise 2 - 27/Checkerboard.java
@@ -0,0 +1,14 @@
+public class Checkerboard {
+	public static void main (String[] args){
+	
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		System.out.println("* * * * * * * *");
+		System.out.println(" * * * * * * * *");
+		
+	}
+}
