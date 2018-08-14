138fac96695c64156d6d16284e3af20765f11680
vejitta <vejitta123@gmail.com>
2018-08-14 21:34:57 +0300

    Exercise 1-6 and 1-10, in which 1-6 the answer is c, and -10 the answer is c -won't compile bec

---
+++ Exercise A/MinMax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; //Imported this class to be anle to use System.in command and erad the inputs from the user
+public class MinMax {
+	public static void main(String[] args) {
+		int i,j,k;
+		Scanner scan = new Scanner(System.in); //I declared the "scan" variable to be able to register the user input
+		
+		System.out.print("What is the first number?\n");
+		i=scan.nextInt();
+		
+		System.out.print("What is the first number?\n");
+		j=scan.nextInt();
+		
+		k=i-j;
+		System.out.println(k);
+		
+		if(k>0)	System.out.println(i+" is the biggest and "+j+" is smallest"); else 
+				System.out.println(i+" is the smallest and "+j+" is the biggest");
+		
+
+
+	}
+}
